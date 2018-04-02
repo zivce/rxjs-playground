dfae9734abd62edb0775bfae2f321660b5648157
zivce <zivkovicmilos5@hotmail.com>
2018-04-02 09:47:26 +0200

    - This contains work on json-server.

--- src/index.js
+++ src/index.js
@@ -13,99 +13,182 @@ let wrapper = document.createElement("div");
 wrapper.className="container";
 document.body.appendChild(wrapper);
 
+
+//select difficulty
+let buttons_group = document.createElement("div");
+buttons_group.className ="buttons_group";
+
+let btn_easy = document.createElement("button");
+btn_easy.className = "btn_config";
+btn_easy.innerHTML = "Easy";
+buttons_group.appendChild(btn_easy);
+
+
+let btn_med = document.createElement("button");
+btn_med.className = "btn_config";
+btn_med.innerHTML = "Medium";
+
+buttons_group.appendChild(btn_med);
+
+
+let btn_hard = document.createElement("button");
+btn_hard.className = "btn_config";
+btn_hard.innerHTML = "Hard";
+
+buttons_group.appendChild(btn_hard);
+
+wrapper.appendChild(buttons_group);
+
 let NUMBER_ENEMIES = 20;//HIGHER = HARDER
 let ENEMIES_SPEED = 100;//LOWER = HARDER
-let DIFFICULTY_ENEMY_HP_DESTRUCTION = 5;//LOWER = HARDER
+let ENEMY_HP_DESTRUCTION = 5;//LOWER = HARDER
 
 //consts    
 let window_height = window.innerHeight - 50;
 
 let Enemies = [];
 let I = 0 ;
-//generating enemies
-Rx.Observable.interval(1000)
-    .subscribe(function(){
-        Enemies.push(new Enemy(wrapper));
-        Enemies[Enemies.length-1].startMoving(ENEMIES_SPEED);
-        I++
-        if(I === NUMBER_ENEMIES)
-            this.unsubscribe();
-
-    },
-    (err)=>{
-        console.log(err)
-    })
-   
-
-//player has joined the game
-let player = new Player(wrapper);
-
-//Check for collision
-
-Rx.Observable.interval(1).subscribe(function(){
-    
-    let hp = player.listenerForCollision(Enemies);
-
-    if(hp <= 0)
-    {
-        
-        this.unsubscribe();                
-    }
-})
 
 
-//shots hit the target  
-Rx.Observable.interval(1).subscribe(function(){
-    
-    player.bullets.forEach(function(bullet){
-        Enemies.forEach(function(enemy){
 
-            //if killed remove from array and continue
+let data_retrieved = false;
+
+btn_easy.onclick = (event)=>{
+    fetch("http://localhost:3000/easy")
+        .then((data)=>{
             
-            if(enemy.health_points === 0)
-            {
-                let remove_enemy_index = Enemies.indexOf(enemy);
-                Enemies.splice(remove_enemy_index,1);
-                enemy.dom_element.parentNode.removeChild(enemy.dom_element);
-                return;
-            }
-            
+            console.log(data.body);
+            let d = data.body;
 
-            let enemy_rect = enemy.dom_element.getBoundingClientRect();
-            
-
-            let bullet_rect = bullet.getBoundingClientRect();
-
-            let x_match = ( Math.abs(bullet_rect.x - (enemy_rect.x+enemy_rect.width/2))) < 50;
-
-
-            let y_match = Math.abs(enemy_rect.y -
-            bullet_rect.y) < 5 ;
-            
-            let bullet_hit_enemy = y_match && x_match ;
-            
-
-            // NANESI STETU
-            if(bullet_hit_enemy)
-            {
-                //console.log("bullet hit!");
-
-                const parent = bullet.parentNode;
-
-                if(parent != null)
-                    parent.removeChild(bullet);
-                player.score += DIFFICULTY_ENEMY_HP_DESTRUCTION;
-                
-                enemy.health_points-= DIFFICULTY_ENEMY_HP_DESTRUCTION;
-                //console.log(enemy.health_points);
-                
-            }
-
+            NUMBER_ENEMIES = d.NUMBER_ENEMIES;
+            ENEMIES_SPEED  = d.ENEMIES_SPEED;
+            ENEMY_HP_DESTRUCTION = d.ENEMY_HP_DESTRUCTION;
+            data_retrieved = true;
         })
-    })
-},
-(err)=>{
-    console.log(err)
-})
+}
 
 
+
+btn_med.onclick = (event)=>{
+    fetch("http://localhost:3000/easy")
+        .then((data)=>{
+            
+            console.log(data.body);
+            let d = data.body;
+
+            NUMBER_ENEMIES = d.NUMBER_ENEMIES;
+            ENEMIES_SPEED  = d.ENEMIES_SPEED;
+            ENEMY_HP_DESTRUCTION = d.ENEMY_HP_DESTRUCTION;
+            data_retrieved = true;
+        })
+}
+
+
+
+
+btn_hard.onclick = (event)=>{
+    fetch("http://localhost:3000/easy")
+        .then((data)=>{
+            console.log(data.body);
+            let d = data.body;
+
+            NUMBER_ENEMIES = d.NUMBER_ENEMIES;
+            ENEMIES_SPEED  = d.ENEMIES_SPEED;
+            ENEMY_HP_DESTRUCTION = d.ENEMY_HP_DESTRUCTION;
+            data_retrieved = true;
+        })
+}
+
+
+if(data_retrieved)
+{
+    
+        //generating enemies
+        Rx.Observable.interval(1000)
+            .subscribe(function(){
+                Enemies.push(new Enemy(wrapper));
+                Enemies[Enemies.length-1].startMoving(ENEMIES_SPEED);
+                I++
+                if(I === NUMBER_ENEMIES)
+                    this.unsubscribe();
+
+            },
+            (err)=>{
+                console.log(err)
+            })
+        
+
+        //player has joined the game
+        let player = new Player(wrapper);
+
+        //Check for collision
+
+        Rx.Observable.interval(1).subscribe(function(){
+
+            let hp = player.listenerForCollision(Enemies);
+
+            if(hp <= 0)
+            {
+                
+                this.unsubscribe();                
+            }
+        })
+
+
+        //shots hit the target  
+        Rx.Observable.interval(1).subscribe(function(){
+            
+            player.bullets.forEach(function(bullet){
+                Enemies.forEach(function(enemy){
+
+                    //if killed remove from array and continue
+                    
+                    if(enemy.health_points === 0)
+                    {
+                        let remove_enemy_index = Enemies.indexOf(enemy);
+                        Enemies.splice(remove_enemy_index,1);
+                        enemy.dom_element.parentNode.removeChild(enemy.dom_element);
+                        return;
+                    }
+                    
+
+                    let enemy_rect = enemy.dom_element.getBoundingClientRect();
+                    
+
+                    let bullet_rect = bullet.getBoundingClientRect();
+
+                    let x_match = ( Math.abs(bullet_rect.x - (enemy_rect.x+enemy_rect.width/2))) < 50;
+
+
+                    let y_match = Math.abs(enemy_rect.y -
+                    bullet_rect.y) < 5 ;
+                    
+                    let bullet_hit_enemy = y_match && x_match ;
+                    
+
+                    // NANESI STETU
+                    if(bullet_hit_enemy)
+                    {
+                        //console.log("bullet hit!");
+
+                        const parent = bullet.parentNode;
+
+                        if(parent != null)
+                            parent.removeChild(bullet);
+                        player.score += ENEMY_HP_DESTRUCTION;
+                        
+                        enemy.health_points-= ENEMY_HP_DESTRUCTION;
+                        //console.log(enemy.health_points);
+                        
+                    }
+
+                })
+            })
+        },
+        (err)=>{
+            console.log(err)
+        })
+
+
+
+}
