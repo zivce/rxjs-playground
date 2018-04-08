1a817fbf32f738f29c8766e8e4e63f2faa6c9770
zivce <zivkovicmilos5@hotmail.com>
2018-04-08 15:10:44 +0200

    - This adds styling for buttons.

--- src/util/buildEndScreen.js
+++ src/util/buildEndScreen.js
@@ -6,11 +6,18 @@ export default function(wrapper,player){
     
     let game_over_text = document.createElement("h1");
     
-    game_over_text.innerText=`GAME OVER!\n Score of ${player.username} is: ${player.score} points`;
-
+    game_over_text.innerText=`GAME OVER!` 
     game_over_text.className="game_over_txt_style";
     wrapper.appendChild(game_over_text);
     
+
+    let prg = document.createElement("p");
+    prg.className = "game_over_parag";
+    
+    prg.innerHTML = `Score of <span>${player.username}</span> is: <span>${player.score}</span> points`;
+    wrapper.appendChild(prg);
+
+
     let buttons_group_end = document.createElement("div");
     buttons_group_end.className ="buttons_group";
 
@@ -27,39 +34,7 @@ export default function(wrapper,player){
     wrapper.appendChild(buttons_group_end);
     
 
-    // let table_scores = document.createElement("table");
-    // table_scores.className = "table_scores";
-
-    // let thead = document.createElement("thead");
-
-    // let tr = document.createElement("tr");
-    // thead.appendChild(tr);
-
-    // let th3 = document.createElement("th");
-    // th3.innerHTML = "Rank";
-    // tr.appendChild(th3);
-
-    // let th1 = document.createElement("th");
-    // th1.innerHTML = "User";
-    // tr.appendChild(th1);
-
-
-    // let th2 = document.createElement("th");
-    // th2.innerHTML = "Scores";
-    // tr.appendChild(th2);
-
-
-    // table_scores.appendChild(thead);
-    
-    // let tbody = document.createElement("tbody");
-    // tbody.className = "scores_body";
-
-    // table_scores.appendChild(tbody);
-
-    // wrapper.appendChild(table_scores);
-
     return {
-        // table_scores:table_scores,
         save:btn_save,
         new:btn_new,
     }
